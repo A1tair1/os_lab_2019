0d5874c7d7a16afbf86eda36a7203035dea9feb1
a1tair <supercat451@gmail.com>
2021-02-23 22:33:23 +0000

    1-2 complete

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -3,5 +3,15 @@
 void RevertString(char *str)
 {
 	// your code here
+    int i;
+    int length = strlen(str);
+    for(i = 0;i < length/2; ++i)
+    {
+        char temp;
+        temp = str[i];
+        str[i] = str[length - 1 - i];
+        str[length - 1 - i] = temp;
+
+    }
 }
 
