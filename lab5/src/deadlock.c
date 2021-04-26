7d2d5a43f91d1bdeba39041129dff0e180524594
A1tair1 <>
2021-04-26 13:34:33 +0000

    deadlock

--- lab5/src/deadlock.c
+++ lab5/src/deadlock.c
@@ -28,25 +28,18 @@ int main() {
             exit(1);
         }
     }
-    printf("Mutex values: ");
-    for (int i = 0; i < COUNT_THREADS; ++i){
-        printf("%d ", var[i]);
-    }
     printf("\nSuccessful completion of the program.\n");
     return 0;
 }
 
-void block(int* mutex_number){
+void block(int* mutex_number)
+{
     int x = (*mutex_number) % COUNT_THREADS;
-    int y = (x + 1) % COUNT_THREADS;
+    int y = (x+1) % COUNT_THREADS;
     pthread_mutex_lock(&mutex[x]);
-    var[x]++;
-    {
-        printf("mutex[%d] has been locked, wait unlock mutex[%d]\n", x, y);
-        pthread_mutex_lock(&mutex[y]);
-        var[y]++;
-        pthread_mutex_unlock(&mutex[y]);
-        printf("mutex[%d] has been unlocked, next - mutex[%d]\n", y, x);
-    }
+    printf("mutex[%d] has been locked, wait unlock mutex[%d]\n", x, y);
+    pthread_mutex_lock(&mutex[y]);
+    pthread_mutex_unlock(&mutex[y]);
+    printf("mutex[%d] has been unlocked, next - mutex[%d]\n", y, x);
     pthread_mutex_unlock(&mutex[x]);
 }
